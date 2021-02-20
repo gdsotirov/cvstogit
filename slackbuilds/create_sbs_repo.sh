d5e1b7ad6bbe223f32ff929617aa3d7571e17f32
Georgi D. Sotirov <gdsotirov@gmail.com>
2021-02-20 12:24:17 +0200

    Add submodules with branch

--- slackbuilds/create_sbs_repo.sh
+++ slackbuilds/create_sbs_repo.sh
@@ -18,22 +18,26 @@ GIT_COMMITTER_DATE="Sat Jan 22 20:31:42 2005 +0200" \
 git commit --no-gpg-sign --date "Sat Jan 22 20:31:42 2005 +0200" \
             -m 'Initial version' COPYING README
 
-git submodule add https://github.com/gdsotirov/net-snmp.SlackBuild.git net-snmp
+git submodule add -b SLACK-10_2 \
+    https://github.com/gdsotirov/net-snmp.SlackBuild.git net-snmp
 GIT_COMMITTER_DATE="Sat Jan 22 20:48:22 2005 +0200" \
 git commit --no-gpg-sign --date "Sat Jan 22 20:48:22 2005 +0200" \
             -m 'Add net-snmp' .gitmodules net-snmp
 
-git submodule add https://github.com/gdsotirov/hddtemp.SlackBuild.git hddtemp
+git submodule add -b SLACK-10_2 \
+    https://github.com/gdsotirov/hddtemp.SlackBuild.git hddtemp
 GIT_COMMITTER_DATE="Thu Mar 03 17:25:16 2005 +0200" \
 git commit --no-gpg-sign --date "Thu Mar 03 17:25:16 2005 +0200" \
             -m 'Add hddtemp' .gitmodules hddtemp
 
-git submodule add https://github.com/gdsotirov/mysql.SlackBuild.git mysql
+git submodule add -b SLACK-10_2 \
+    https://github.com/gdsotirov/mysql.SlackBuild.git mysql
 GIT_COMMITTER_DATE="Fri Mar 11 23:01:00 2005 +0200" \
 git commit --no-gpg-sign --date "Fri Mar 11 23:01:00 2005 +0200" \
             -m 'Add mysql' .gitmodules mysql
 
-git submodule add https://github.com/gdsotirov/php.SlackBuild php
+git submodule add -b SLACK-10_2 \
+    https://github.com/gdsotirov/php.SlackBuild php
 GIT_COMMITTER_DATE="Sat Mar 12 15:06:01 2005 +0200" \
 git commit --no-gpg-sign --date "Sat Mar 12 15:06:01 2005 +0200" \
             -m 'Add php' .gitmodules php
